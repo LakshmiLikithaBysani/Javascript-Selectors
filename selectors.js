2dac4a89886d71e047000ed02e6e8fc5d9838393
LakshmiLikithaBysani <162402980+LakshmiLikithaBysani@users.noreply.github.com>
2026-09-24 21:04:34 +0530

    Add files via upload

---
+++ selectors.js
@@ -0,0 +1,7 @@
+const heading = document.getElementById("heading");
+const paragraph = document.querySelector(".text");
+const button = document.querySelector("button");
+
+console.log(heading.innerText); // Heading
+console.log(paragraph.innerText); // Paragraph
+console.log(button.innerText); // Click Me
